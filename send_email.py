85a1816283e16d9863c5ddf1ae0dfc158ac618cf
DogiparthiAasrith <141314342+DogiparthiAasrith@users.noreply.github.com>
2025-10-13 20:16:11 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 # ===============================
 # CONFIGURATION
 # ===============================
-POSTGRES_URL = "postgresql://postgres:#Aasrith217@localhost:5432/data_ds"
+POSTGRES_URL = "postgresql://neondb_owner:npg_onVe8gqWs4lm@ep-solitary-bush-addf9gpm-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 try:
     # Recommended for deployment: set secrets in Streamlit Cloud
@@ -202,4 +202,5 @@ def main():
 # RUN APP
 # ===============================
 if __name__ == "__main__":
+
     main()
